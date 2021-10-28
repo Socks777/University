070d040fd528cb3042c82808f07415f8442eb858
Socks777 <montgomeryf.sykes@gmail.com>
2021-10-28 13:12:42 +1300

    Assignment 3

---
+++ Assets/Shop.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+public class Shop : MonoBehaviour {
+
+	public TurretBlueprint standardTurret;
+	public TurretBlueprint cannonLauncher;
+	public TurretBlueprint crystalBeamer;
+
+	BuildManager buildManager;
+
+	void Start ()
+	{
+		buildManager = BuildManager.instance;
+	}
+	public void SelectStandardTurret ()
+	{
+		Debug.Log("Standard Turret Selected");
+		buildManager.SelectTurretToBuild(standardTurret);
+	}
+	public void SelectCannonLauncher()
+	{
+		Debug.Log("Cannon Launcher Selected");
+		buildManager.SelectTurretToBuild(cannonLauncher);
+	}
+
+	public void SelectLaserBeamer()
+	{
+		Debug.Log("Crystal Beamer Selected");
+		buildManager.SelectTurretToBuild(crystalBeamer);
+	}
+
+}
---
+++ Assets/TurretBlueprint.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable] //saves and loads the values for me and making ti editable
+public class TurretBlueprint {
+
+	public GameObject prefab;
+	public int cost;
+	public GameObject upgradedPrefab;
+	public int upgradeCost;
+
+	public int GetSellAmount ()
+	{
+		return cost / 2;
+	}
+
+
+}
